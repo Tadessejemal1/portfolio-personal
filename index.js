52d65841cd912482f22029d85c1cef242a988e6c
Tadesse Jemal <tadessejemal63@gmail.com>
2022-07-19 16:53:06 +0300

    Add js function on mobile menu

--- index.js
+++
@@ -1,9 +0,0 @@
-const mobileNavMenu = document.querySelector('.mobile-nav-menu');
-const MenuClose = document.querySelector('.btn-menu-close');
-const navToggle = document.querySelector('.navbar-toggle');
-const navLinks = document.querySelectorAll('.nav-link');
-
-navToggle.addEventListener("click", () => {
-    navToggle.classList.toggle("active");
-    navLinks.classList.toggle("active");
-})
